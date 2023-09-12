80fcb7370854a5f774cea525a58d2f7e3a279e21
Guillermo Jimenez <guillermojimenez@Guillermos-MacBook-Pro-2.local>
2023-09-12 10:57:16 -0400

    working with button icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { View, StyleSheet, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../components/game/NumberContainer";
 import Title from "../components/ui/Title";
@@ -66,12 +67,12 @@ function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "higher")}>
-              +
+              <Ionicons name="md-add" size={24} color="white"></Ionicons>
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
+              <Ionicons name="md-remove" size={24} color="white"></Ionicons>
             </PrimaryButton>
           </View>
         </View>
